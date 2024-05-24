4670a9a4ba42494b6e42ac6703a59a226c17a377
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-05-24 16:00:00 +0200

    working map with user location and PI

--- resources/js/components/App/AppMapLibre.vue
+++ resources/js/components/App/AppMapLibre.vue
@@ -1,13 +1,14 @@
 <script setup>
+import { onMounted, onUnmounted, reactive, ref, watch } from "vue";
 import "maplibre-gl/dist/maplibre-gl.css";
+import mapboxgl from "maplibre-gl";
+import { defineProps, defineModel } from "vue";
 import { MapboxOverlay } from "@deck.gl/mapbox";
 import { ScatterplotLayer } from "@deck.gl/layers";
-import { PathLayer } from "deck.gl";
-import { Map, NavigationControl } from "maplibre-gl";
-import { reactive, onMounted, computed } from "vue";
-
-import { store } from "@/Stores/userPosition";
 import { useGeolocation } from "@vueuse/core";
+
+const INTEREST_POINT_COLOR = [255, 0, 255];
+const INTEREST_POINT_SIZE = 10;
 
 const props = defineProps({
     interestPoints: {
@@ -16,50 +17,99 @@ const props = defineProps({
     },
 });
 
-const { coords, locatedAt, error, resume, pause } = useGeolocation();
+const mapInstance = ref(null);
+const model = defineModel();
 
-const emit = defineEmits(["clickOnPoint"]);
+const interestPoints = ref(props.interestPoints);
+const interestPointsMarkers = ref([]);
 
-const IP_COLOR = [255, 0, 255];
+watch(
+    () => model.value.zoom,
+    (zoom) => {
+        if (mapInstance.value) {
+            mapInstance.value.setZoom(zoom);
+            mapInstance.value.setCenter([model.value.lng, model.value.lat]);
+        }
+    },
+);
 
-const ipDataReact = reactive([]);
+const updateLocation = () => {
+    model.value = getLocation();
+};
 
-props.interestPoints.forEach((ip) => {
-    ipDataReact.push({
-        position: [+ip.long, +ip.lat],
-        color: IP_COLOR,
-    });
-});
+const getLocation = () => {
+    return {
+        ...mapInstance.value.getCenter(),
+        bearing: mapInstance.value.getBearing(),
+        pitch: mapInstance.value.getPitch(),
+        zoom: mapInstance.value.getZoom(),
+    };
+};
 
 onMounted(() => {
-    const map = new Map({
-        container: "map",
-        style: "https://vectortiles.geo.admin.ch/styles/ch.swisstopo.lightbasemap.vt/style.json",
-        center: [6.647398, 46.781334],
-        zoom: 15,
+    const map = new mapboxgl.Map({
+        container: "mapContainer",
+        style: "https://vectortiles.geo.admin.ch/styles/ch.swisstopo.lightbasemap.vt/style.json", // Replace with your preferred map style
+        center: [model.value.lng, model.value.lat],
+        zoom: model.value.zoom,
     });
 
-    const layer = new ScatterplotLayer({
-        id: "point-layer",
-        data: ipDataReact,
-        getPosition: (d) => d.position,
-        getRadius: 10,
-        getFillColor: (d) => d.color,
+    const geolocateControl = new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true,
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true,
+        showUserLocation: true,
     });
 
-    const mapboxOverlay = new MapboxOverlay({ layers: [layer] });
+    const navigationControl = new mapboxgl.NavigationControl();
 
-    map.addControl(mapboxOverlay);
+    map.addControl(geolocateControl);
+    map.addControl(navigationControl);
+
+    map.on("load", function () {
+        geolocateControl.trigger();
+    });
+
+    map.on("load", () => {
+        interestPoints.value.forEach((interestPoint) => {
+            const interestPointMarker = new mapboxgl.Marker({
+                color: INTEREST_POINT_COLOR,
+                draggable: false,
+            })
+                .setLngLat([interestPoint.long, interestPoint.lat])
+                .addTo(map);
+
+            interestPointsMarkers.value.push(interestPointMarker);
+        });
+    });
+
+    // Trop lourd pour le moment (fait ralentire le zoom et le déplacement de la carte)
+    // TODO: à optimiser
+    // map.on("move", updateLocation);
+    // map.on("zoom", updateLocation);
+    // map.on("rotate", updateLocation);
+    // map.on("pitch", updateLocation);
+
+    mapInstance.value = map;
+});
+
+onUnmounted(() => {
+    mapInstance.value.remove();
+    mapInstance.value = null;
 });
 </script>
 
 <template>
-    {{ coords.latitude }}, {{ coords.longitude }}
-    <div id="map"></div>
+    <div id="mapContainer" class="map-container"></div>
 </template>
 
 <style scoped>
-#map {
-    height: 100vh;
+.map-container {
+    flex: 1;
+    height: 97vh;
 }
 </style>
