797433270bc6ec4da6215d872b5b72f7cf0c7032
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-05-27 15:57:47 +0200

    implementation of collection page

---
+++ resources/js/Components/App/AppBadge.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+    icon: {
+        type: String,
+        default: "default.png",
+    },
+    badge: {
+        type: String,
+        required: true,
+    },
+    count: {
+        type: Number,
+        required: true,
+    },
+    total: {
+        type: Number,
+        reuiqred: true,
+    },
+});
+</script>
+
+<template>
+    <div class="flex flex-col items-center">
+        <div
+            class="border border-neutral-200 w-20 h-20 bg-neutral-100 shadow-md rounded-md flex items-center justify-center mb-2"
+        >
+            <img :src="'storage/badges/' + icon" alt="" srcset="" />
+        </div>
+        <div class="max-w-20">
+            <p class="truncate overflow-hidden">{{ badge }}</p>
+        </div>
+        <div v-if="count && total" class="text-sm text-base-300">
+            {{ count }} / {{ total }}
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
