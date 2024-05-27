4f0e270357d60f3c67d618a5a5bc0891feadb22e
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-05-27 13:56:47 +0200

    updated layout to match ui concept

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -3,8 +3,10 @@ import TheNavBar from "@/Components/The/TheNavBar.vue";
 </script>
 
 <template>
-    <slot name="main" />
-    <TheNavBar />
+    <div class="flex flex-col sm:justify-center items-center bg-gray-100">
+        <slot name="main" />
+        <TheNavBar />
+    </div>
 </template>
 
 <style scoped></style>
