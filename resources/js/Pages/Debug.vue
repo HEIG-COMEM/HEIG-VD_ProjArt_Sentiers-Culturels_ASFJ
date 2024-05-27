e30dbe64de8dafb3127f0e1db4eb615fc2c569f7
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-05-27 15:58:40 +0200

    cleared debug

--- resources/js/Pages/Debug.vue
+++ resources/js/Pages/Debug.vue
@@ -1,25 +1,5 @@
-<script setup>
-import { ref } from "vue";
+<script setup></script>
 
-import MapPane from "@/Components/MapPane.vue";
-import MapDeckGL from "@/Components/MapDeckGL.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
-
-const props = defineProps({
-    counter: Number,
-});
-
-const counter = ref(props.counter);
-</script>
-
-<template>
-    <div class="flex flex-col items-center">
-        <h1 class="text-2xl">Debug</h1>
-        <p>{{ counter }}</p>
-        <PrimaryButton @click="counter++">Increment</PrimaryButton>
-    </div>
-    <!-- <MapPane /> -->
-    <MapDeckGL />
-</template>
+<template></template>
 
 <style scoped></style>
