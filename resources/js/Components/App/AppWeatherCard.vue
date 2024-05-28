38d24299054a6e90482a311eb1c62ca7ae277388
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-05-29 00:11:51 +0200

    WIP on route-details

---
+++ resources/js/Components/App/AppWeatherCard.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { ref, defineProps, onMounted } from "vue";
+import WMO from "../../utils/WMO.json";
+
+const props = defineProps({
+    lat: {
+        type: Number,
+        reqired: true,
+    },
+    long: {
+        type: Number,
+        reqired: true,
+    },
+});
+
+const weather = ref(null);
+const isLoading = ref(true);
+const error = ref(null);
+
+const getWeatherIcon = (code) => {
+    return WMO[code].day.image;
+};
+
+const getWeather = (weather) => {
+    const weatherData = [];
+    for (let i = 0; i < weather.time.length; i++) {
+        weatherData.push({
+            day: weather.time[i],
+            icon: getWeatherIcon(weather.weather_code[i]),
+            maxTemp: weather.temperature_2m_max[i],
+            minTemp: weather.temperature_2m_min[i],
+            rain: weather.rain_sum[i],
+            snow: weather.snowfall_sum[i],
+        });
+    }
+    return weatherData;
+};
+
+const fetchAPI = async () => {
+    const API_URL = `https://api.open-meteo.com/v1/forecast?latitude=${props.lat}&longitude=${props.long}&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,showers_sum,snowfall_sum&timezone=Europe%2FBerlin&forecast_days=3`;
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data;
+};
+
+onMounted(() => {
+    fetchAPI().then((data) => {
+        console.log(data);
+        if (data) {
+            weather.value = getWeather(data.daily);
+        } else {
+            error.value = data.status;
+        }
+        isLoading.value = false;
+    });
+});
+</script>
+
+<template>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else-if="error">
+        {{ error }}
+    </div>
+    <div v-else>
+        <div v-for="item in weather" :key="item.day">
+            <div>day {{ item.day }}</div>
+            <img :src="item.icon" alt="weather icon" />
+            <div>maxTemp {{ item.maxTemp }}</div>
+            <div>minTemp {{ item.minTemp }}</div>
+            <div>rain {{ item.rain }}</div>
+            <div>snow {{ item.snow }}</div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
--- resources/js/Pages/Route.vue
+++ resources/js/Pages/Route.vue
@@ -1,15 +1,22 @@
 <script setup>
 import { Head, Link } from "@inertiajs/vue3";
-import { defineProps, ref } from "vue";
+import { computed, defineProps, reactive, ref } from "vue";
 import {
     ArrowLeft,
+    ArrowDown,
     Star,
     LoginVertical as Download,
     Sun,
+    Clock3,
+    Activity,
+    MenuHamburger3,
+    Tag,
 } from "@iconsans/vue/linear";
 import { Star as StarFull } from "@iconsans/vue/bold";
 
 import MobileAppLayout from "@/Layouts/AppLayout.vue";
+import AppStarRating from "@/Components/App/AppStarRating.vue";
+import AppWeatherCard from "@/Components/App/AppWeatherCard.vue";
 
 const props = defineProps({
     route: {
@@ -17,7 +24,40 @@ const props = defineProps({
         required: true,
     },
 });
-const bgImgPath = `/storage/pictures/${props.route.pictures.at(0).path}`;
+
+const route = ref(props.route);
+
+const getImgPath = (path) => `/storage/pictures/${path}`;
+const bgImgPath = computed(() => getImgPath(route.value.pictures.at(0).path));
+
+const duration = computed(() => {
+    return (
+        Math.floor(route.value.duration / 60) +
+        "h" +
+        (route.value.duration % 60 === 0
+            ? ""
+            : " " + (route.value.duration % 60) + "m")
+    );
+});
+
+const distance = computed(() => {
+    return route.value.length > 1000
+        ? (route.value.length / 1000).toFixed(2) + " km"
+        : route.value.length + " m";
+});
+
+const rates = ref(route.value.rates);
+
+const rate = computed(() => {
+    return (
+        rates.value.reduce((acc, rate) => acc + rate.rate, 0) /
+        rates.value.length
+    );
+});
+
+const roundedRate = computed(() => (Math.round(rate.value * 2) / 2).toFixed(1));
+
+const interestPoints = reactive(route.value.interest_points);
 
 const isFavorite = ref(false); // TODO: Bind with user favorites
 const showWeather = ref(false);
@@ -29,12 +69,35 @@ const back = () => {
     }
     window.history.back();
 };
+
+const scrollToNext = (event) => {
+    event.preventDefault();
+    document.querySelector(".scroll-container").scrollBy({
+        top: window.innerHeight,
+        behavior: "smooth",
+    });
+};
+
+// TODO: Remove
+console.log(route.value);
 </script>
 
 <template>
     <Head title="Sentier" />
     <MobileAppLayout>
         <template v-slot:main>
+            <!-- WEATHER -->
+            <div
+                v-if="showWeather"
+                class="absolute z-[9999] p-6 top-0 right-0 w-full h-full"
+            >
+                <AppWeatherCard
+                    :lat="+route.start_lat"
+                    :long="+route.start_long"
+                />
+            </div>
+
+            <!-- TOP BTNS -->
             <div class="absolute z-[1] p-6 top-0 left-0 w-full">
                 <div class="flex flex-row justify-between">
                     <div>
@@ -63,7 +126,6 @@ const back = () => {
                         </button>
                         <button
                             class="btn btn-circle btn-outline btn-primary bg-base-100 border-none"
-                            @click="back()"
                         >
                             <Sun
                                 class="w-7 h-7"
@@ -73,25 +135,158 @@ const back = () => {
                     </div>
                 </div>
             </div>
+
+            <!-- BOTTOM BTNS -->
+            <div class="absolute z-[1] p-6 bottom-16 left-0 w-full">
+                <div class="flex flex-col items-center gap-2">
+                    <button
+                        class="btn btn-circle btn-outline btn-primary bg-base-100 border-none"
+                        @click="scrollToNext($event)"
+                    >
+                        <ArrowDown class="h-7 w-7" />
+                    </button>
+                    <Link href="#" class="btn btn-primary w-full">
+                        Commencer la randonnée
+                    </Link>
+                </div>
+            </div>
             <div
                 class="background-image w-full h-full bg-primary flex flex-col justify-center"
             >
-                <div class="backdrop-blur-md w-full h-full">
+                <div
+                    class="scroll-container backdrop-blur-md w-full h-full overflow-scroll"
+                >
+                    <!-- ROUTE -->
                     <div
                         class="flex flex-col justify-center items-center h-full gap-8 p-16"
                     >
-                        <img :src="bgImgPath" class="w-36 h-36 rounded-md" />
+                        <img
+                            :src="bgImgPath"
+                            :alt="route.pictures.at(0).title"
+                            class="w-36 h-36 rounded-md"
+                        />
                         <div class="flex flex-col items-center">
-                            <h1 class="text-2xl font-bold text-base-100">
+                            <h1
+                                class="text-2xl font-bold text-base-100 text-center"
+                            >
                                 {{ route.name }}
                             </h1>
                             <p
-                                class="text-sm font-semibold mt-2 px-4 text-center text-base-100"
+                                class="text-sm font-semibold mt-2 px-4 text-center text-base-100 max-h-32 overflow-scroll"
                             >
                                 {{ route.description }}
                             </p>
                         </div>
                     </div>
+
+                    <!-- GENERAL INFO -->
+                    <div
+                        class="flex flex-col justify-center items-center h-full gap-8 p-16"
+                    >
+                        <div class="flex flex-col items-center gap-10">
+                            <h1 class="text-2xl font-bold text-base-100">
+                                Informations générales
+                            </h1>
+                            <div class="flex flex-col w-full gap-4">
+                                <div class="flex flex-row items-center gap-4">
+                                    <Clock3 class="h-7 w-7 text-base-100" />
+                                    <p
+                                        class="text-base-100 text-base font-semibold"
+                                    >
+                                        {{ duration }}
+                                    </p>
+                                </div>
+                                <div class="flex flex-row items-center gap-4">
+                                    <Activity class="h-7 w-7 text-base-100" />
+                                    <p
+                                        class="text-base-100 text-base font-semibold"
+                                    >
+                                        {{ distance }}
+                                    </p>
+                                </div>
+                                <div class="flex flex-row items-center gap-4">
+                                    <MenuHamburger3
+                                        class="h-7 w-7 text-base-100 -rotate-90 self-start"
+                                    />
+                                    <p
+                                        class="text-base-100 text-base font-semibold"
+                                    >
+                                        Difficulté {{ route.difficulty.name }}
+                                    </p>
+                                </div>
+                                <div class="flex flex-row items-center gap-4">
+                                    <Tag
+                                        class="h-7 w-7 text-base-100 self-start"
+                                    />
+                                    <div
+                                        class="text-base-100 text-base font-semibold flex flex-col gap-2 max-h-24 overflow-scroll flex-grow"
+                                    >
+                                        <div
+                                            v-for="tag in route.tags"
+                                            class="badge badge-primary"
+                                        >
+                                            {{ tag.name }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- RATING -->
+                    <div
+                        class="flex flex-col justify-center items-center h-full gap-8 p-16"
+                    >
+                        <div class="flex flex-col items-center gap-10">
+                            <h1 class="text-2xl font-bold text-base-100">
+                                Note du sentier
+                            </h1>
+                            <div class="flex flex-col items-center gap-2">
+                                <p class="text-base-100">
+                                    <span class="text-6xl">{{
+                                        roundedRate
+                                    }}</span
+                                    ><span>/5</span>
+                                </p>
+                                <AppStarRating :rating="+roundedRate" />
+                                <p class="text-base-100 text-sm">
+                                    ({{ route.rates.length }} avis)
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- INTEREST POINTS -->
+                    <template v-for="(interestPoint, index) in interestPoints">
+                        <div
+                            class="flex flex-col justify-center items-center h-full gap-8 p-16"
+                        >
+                            <p class="text-base-100">
+                                {{ index + 1 }} / {{ interestPoints.length }}
+                            </p>
+                            <img
+                                :src="
+                                    getImgPath(
+                                        interestPoint.pictures.at(0).path,
+                                    )
+                                "
+                                :alt="interestPoint.pictures.at(0).title"
+                                class="w-36 h-36 rounded-md"
+                            />
+                            <div class="flex flex-col items-center">
+                                <h1
+                                    class="text-2xl font-bold text-base-100 text-center"
+                                >
+                                    {{ interestPoint.name }}
+                                </h1>
+                                <p
+                                    class="text-sm font-semibold mt-2 px-4 text-center text-base-100 max-h-32 overflow-scroll"
+                                >
+                                    {{ interestPoint.description }}
+                                </p>
+                            </div>
+                        </div>
+                    </template>
                 </div>
             </div>
         </template>
@@ -106,5 +301,13 @@ const back = () => {
     background-repeat: no-repeat;
     height: 100vh;
     width: 100vw;
+}
+
+.scroll-container {
+    scroll-snap-type: y mandatory;
+}
+
+.scroll-container > * {
+    scroll-snap-align: center;
 }
 </style>
