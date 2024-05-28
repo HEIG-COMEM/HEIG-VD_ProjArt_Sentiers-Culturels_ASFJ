da9419962bb4898ac81e484be1d2c60ec544575b
antoineuldry <antoine.uldry@heig-vd.ch>
2024-05-28 16:24:32 +0200

    Created component Favorite Card

--- resources/js/Pages/Favorite.vue
+++ resources/js/Pages/Favorite.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { Head } from "@inertiajs/vue3";
 import MobileAppLayout from "@/Layouts/AppLayout.vue";
+import AppHorizontalCard from "@/Components/App/AppHorizontalCard.vue";
+import { Tick2, ArrowRight2 } from "@iconsans/vue/linear";
+import { Link } from "@inertiajs/vue3";
 
 const getUserFavorites = () => {
     // fetch the user's favorite sentiers from the database
@@ -12,23 +15,46 @@ const getUserFavorites = () => {
     <Head title="Favoris" />
     <MobileAppLayout>
         <template v-slot:main>
-            <div class="card card-side bg-base-100 shadow-xl">
-                <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
-                        alt="Album"
-                    />
-                </figure>
-                <div class="card-body">
-                    <h2 class="card-title">New album is released!</h2>
-                    <p>Click the button to listen on Spotiwhy app.</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Listen</button>
-                    </div>
-                </div>
+            <div class="h-full w-full p-6 flex flex-col gap-2">
+                <AppHorizontalCard
+                    title="Parcours sentier"
+                    tag="Lorem"
+                    href="/"
+                    img-path="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+                    img-alt="lorem"
+                    :is-active="true"
+                />
+                <AppHorizontalCard
+                    title="Parcours sentier"
+                    tag="Lorem"
+                    href="/"
+                    img-path="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+                    img-alt="lorem"
+                />
+                <AppHorizontalCard
+                    title="Parcours sentier"
+                    tag="Lorem"
+                    href="/"
+                    img-path="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+                    img-alt="lorem"
+                />
+                <AppHorizontalCard
+                    title="Parcours sentier"
+                    tag="Lorem"
+                    href="/"
+                    img-path="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+                    img-alt="lorem"
+                />
             </div>
         </template>
     </MobileAppLayout>
 </template>
 
-<style scoped></style>
+<style scoped>
+.indicator-item {
+    transform: translate(20%, -20%);
+}
+
+.badge {
+}
+</style>
