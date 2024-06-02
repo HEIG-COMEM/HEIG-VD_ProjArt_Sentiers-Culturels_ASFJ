f612a7a52b436f45a26a5c501fa0a5044f32d24a
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-06-02 22:44:28 +0200

    backoffice collection page w filter
    require review

--- app/Http/Controllers/CollectionAdminController.php
+++ app/Http/Controllers/CollectionAdminController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\DifficultyResource;
 use App\Http\Resources\InterestPointResource;
 use App\Http\Resources\RouteResource;
+use App\Http\Resources\TagResource;
+use App\Models\Difficulty;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\Route;
@@ -19,11 +23,17 @@ class CollectionAdminController extends Controller
         $routes->load('tags');
 
         $interestpoints = InterestPoint::all();
+        $interestpoints->load('pictures');
+
+        $difficulties = Difficulty::all();
+        $tags = Tag::all();
 
 
         return Inertia::render('Backoffice/Collection', [
             'routes' => RouteResource::collection($routes),
             'interestpoints' => InterestPointResource::collection($interestpoints),
+            'difficulties' => DifficultyResource::collection($difficulties),
+            'tags' => TagResource::collection($tags),
         ]);
     }
 }
--- resources/js/Pages/Backoffice/Collection.vue
+++ resources/js/Pages/Backoffice/Collection.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { reactive, ref, computed } from "vue";
 import { Head } from "@inertiajs/vue3";
-import { ArrowDown2 } from "@iconsans/vue/linear";
 
 import BackofficeLayout from "@/Layouts/BackofficeLayout.vue";
 import BaseSearchBar from "@/Components/Base/BaseSearchBar.vue";
 import AppHorizontalCard from "@/Components/App/AppHorizontalCard.vue";
+
+import { ArrowDown2 } from "@iconsans/vue/linear";
 
 const props = defineProps({
     interestpoints: {
@@ -13,6 +14,14 @@ const props = defineProps({
         required: true,
     },
     routes: {
+        type: Object,
+        required: true,
+    },
+    difficulties: {
+        type: Object,
+        required: true,
+    },
+    tags: {
         type: Object,
         required: true,
     },
@@ -25,18 +34,64 @@ const props = defineProps({
 
 const routes = reactive(props.routes.data);
 const interestpoints = reactive(props.interestpoints.data);
+const difficulties = reactive(props.difficulties.data);
+const tags = reactive(props.tags.data);
 
 const showRoutes = ref(props.displayType === "routes");
 const routeSearch = ref("");
+const interestpointSearch = ref("");
+
+const selectedDifficulty = ref(null);
+const selectedTags = reactive([]);
 
 const getImgPath = (path) => `/storage/pictures/${path}`;
 const getTagsName = (tags) => tags.map((tag) => tag.name);
 
 const filteredRoutes = computed(() => {
-    return routes.filter((route) =>
+    let filter = routes.filter((route) =>
         route.name.toLowerCase().includes(routeSearch.value.toLowerCase()),
     );
+
+    if (selectedDifficulty.value) {
+        filter = filter.filter(
+            (route) => route.difficulty_id === selectedDifficulty.value,
+        );
+    }
+
+    if (selectedTags.length) {
+        selectedTags.forEach((tag) => {
+            filter = filter.filter((route) =>
+                route.tags.some((routeTag) => routeTag.id === tag),
+            );
+        });
+    }
+
+    return filter;
 });
+
+const filteredInterestPoints = computed(() => {
+    return interestpoints.filter((interestpoint) =>
+        interestpoint.name
+            .toLowerCase()
+            .includes(interestpointSearch.value.toLowerCase()),
+    );
+});
+
+const handleTagSelection = (tagId) => {
+    if (selectedTags.includes(tagId)) {
+        selectedTags.splice(selectedTags.indexOf(tagId), 1);
+    } else {
+        selectedTags.push(tagId);
+    }
+};
+
+const hasFiltersApplied = computed(() => {
+    if (selectedDifficulty.value || selectedTags.length) return true;
+});
+const clearFilters = () => {
+    selectedDifficulty.value = null;
+    selectedTags.splice(0);
+};
 </script>
 
 <template>
@@ -96,19 +151,59 @@ const filteredRoutes = computed(() => {
                             v-model="routeSearch"
                         />
                         <div
-                            class="flex flex-row gap-2 w-full overflow-y-scroll"
+                            class="flex flex-row gap-2 w-full overflow-y-scroll relative"
                         >
-                            <div class="badge badge-primary gap-2">
-                                <ArrowDown2
-                                    class="inline-block w-4 h-4 stroke-current"
-                                />
-                                success
+                            <div
+                                class="flex flex-row gap-2 w-full overflow-y-scroll relative"
+                            >
+                                <div
+                                    class="rounded-lg flex flex-row gap-2 items-center bg-base-200 p-2 text-xs cursor-pointer"
+                                    onclick="difficulty_modal.showModal()"
+                                    :class="{
+                                        'bg-primary': selectedDifficulty,
+                                        'text-primary-content':
+                                            selectedDifficulty,
+                                    }"
+                                >
+                                    Difficulté
+                                    <span class="h-4 w-4"
+                                        ><ArrowDown2 class="h-full w-full"
+                                    /></span>
+                                </div>
+                                <div
+                                    class="rounded-lg flex flex-row gap-2 items-center bg-base-200 p-2 text-xs cursor-pointer"
+                                    onclick="tags_modal.showModal()"
+                                    :class="{
+                                        'bg-primary': selectedTags.length,
+                                        'text-primary-content':
+                                            selectedTags.length,
+                                    }"
+                                >
+                                    Tags
+                                    {{
+                                        selectedTags.length
+                                            ? `(${selectedTags.length})`
+                                            : ""
+                                    }}
+                                    <span class="h-4 w-4"
+                                        ><ArrowDown2 class="h-full w-full"
+                                    /></span>
+                                </div>
+                                <div
+                                    v-show="hasFiltersApplied"
+                                    class="rounded-lg bg-base-200 p-2 text-xs cursor-pointer"
+                                    @click="clearFilters()"
+                                >
+                                    Supprimer les filtres
+                                </div>
                             </div>
                         </div>
                     </div>
 
                     <!-- ROUTES LIST -->
-                    <div class="flex flex-col gap-4 w-full">
+                    <div
+                        class="flex flex-col gap-4 w-full max-h-[70vh] px-3 pb-6 overflow-x-scroll"
+                    >
                         <AppHorizontalCard
                             v-for="route in filteredRoutes"
                             :key="route.id"
@@ -119,9 +214,105 @@ const filteredRoutes = computed(() => {
                             :tags="getTagsName(route.tags)"
                             :href="`/backoffice/routes/${route.uuid}`"
                         />
+                        <p
+                            v-if="filteredRoutes.length === 0"
+                            class="text-center text-base-200"
+                        >
+                            Aucun résultat
+                        </p>
+                    </div>
+                </div>
+
+                <!-- INTEREST POINTS -->
+                <div
+                    v-show="!showRoutes"
+                    class="flex flex-col gap-8 justify-center w-full"
+                >
+                    <!-- FILTER SECTION -->
+                    <div class="flex flex-col justify-center w-full gap-4">
+                        <BaseSearchBar
+                            :is-absolute="false"
+                            placeholder="Rechercher"
+                            class="w-full"
+                            v-model="interestpointSearch"
+                        />
+                    </div>
+
+                    <!-- INTEREST POINTS LIST -->
+                    <div
+                        class="flex flex-col gap-4 w-full max-h-[70vh] px-3 pb-6 overflow-x-scroll"
+                    >
+                        <AppHorizontalCard
+                            v-for="interestpoint in filteredInterestPoints"
+                            :key="interestpoint.id"
+                            :title="interestpoint.name"
+                            :img-path="
+                                getImgPath(interestpoint.pictures.at(0).path)
+                            "
+                            :img-alt="interestpoint.name"
+                            :display-is-done="false"
+                            :href="
+                                route(
+                                    'backoffice.interest-points.show',
+                                    interestpoint.uuid,
+                                )
+                            "
+                        />
                     </div>
                 </div>
             </div>
+
+            <dialog id="difficulty_modal" class="modal">
+                <div class="modal-box">
+                    <h3 class="font-bold text-lg">Difficultés</h3>
+                    <div class="py-4">
+                        <div
+                            v-for="difficulty in difficulties"
+                            :key="difficulty.id"
+                            class="flex flex-row gap-2 items-center mb-2"
+                            @click="selectedDifficulty = difficulty.id"
+                        >
+                            <input
+                                type="radio"
+                                :checked="selectedDifficulty === difficulty.id"
+                                class="radio"
+                                name="difficulty"
+                            />
+                            <label :for="difficulty.id">{{
+                                difficulty.name
+                            }}</label>
+                        </div>
+                    </div>
+                </div>
+                <form method="dialog" class="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
+
+            <dialog id="tags_modal" class="modal">
+                <div class="modal-box">
+                    <h3 class="font-bold text-lg mb-2">Tags</h3>
+                    <div class="py-4 max-h-[70vh] overflow-x-scroll">
+                        <div
+                            v-for="tag in tags"
+                            :key="tag.id"
+                            class="flex flex-row gap-2 items-center mb-2 px-2"
+                            @click="handleTagSelection(tag.id)"
+                        >
+                            <input
+                                type="checkbox"
+                                :checked="selectedTags.includes(tag.id)"
+                                class="checkbox"
+                                name="tag"
+                            />
+                            <label :for="tag.id">{{ tag.name }}</label>
+                        </div>
+                    </div>
+                </div>
+                <form method="dialog" class="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
         </template>
     </BackofficeLayout>
 </template>
