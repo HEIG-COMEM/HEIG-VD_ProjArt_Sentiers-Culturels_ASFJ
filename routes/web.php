bf9e790907b09c9d0ef873f7db40b42de8dde61c
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-06-06 20:33:06 +0200

    custom error page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use function Laravel\Prompts\error;
+
 Route::get('/', function () {
     return redirect()->route('home');
 });
